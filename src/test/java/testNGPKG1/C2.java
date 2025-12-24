aa204ffc78be251a83de03bdf997b4cbb1666a92
unknown <eraman.kush@gmail.com>
2025-12-24 13:59:44 +0530

    TestNG Code

---
+++ src/test/java/testNGPKG1/C2.java
@@ -0,0 +1,15 @@
+package testNGPKG1;
+
+
+import org.testng.annotations.Test;
+
+public class C2 {
+  @Test
+    public void M3(){
+        System.out.println("M3->E");
+    }
+    @Test
+    public void M4(){
+        System.out.println("M4->F");
+    }
+}
